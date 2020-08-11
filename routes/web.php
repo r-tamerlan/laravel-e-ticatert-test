0b765c6e1053e0988cb67b0886e6a0ac1a5a9101
r-tamerlan <59124933+r-tamerlan@users.noreply.github.com>
2020-08-11 12:39:29 +0400

    Controller Yapisi

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','homepageController@index')->name('homepage');
 
-Route::get("/urun/{urunadi}",function ($urunadi){
-   return "urun adi: $urunadi";
-});
